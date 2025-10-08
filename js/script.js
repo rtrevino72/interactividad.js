8b7490a210c36d01f584d19531639a643563e724
rtrevino72 <rtrevino@centro.edu.mx>
2025-10-08 10:59:48 -0600

    variables

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// asi se declara una variable
+var nombre = "Regina"; // string - tipo cadena de texto
+let edad = 36;
+const PI = 3.14159265358979323846;
+
+// Valor booleano - Tipos de variable que pueden ser true o false
+let esMayorDeEdad = edad >= 18; // Variable formato camelcase
+
+nombre = "Luis"
+edad = 37;
+console.log("Hola" + nombre);
